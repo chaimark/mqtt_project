e2c375531c5e2cda187ecc6045b3d53e01469a1c
chai21sks <chai21sks@gmail.com>
2026-05-25 14:55:42 +0800

    Mon May 25 14:55:42 CST 2026

--- mqtt_client/install_mqtt.sh
+++ mqtt_client/install_mqtt.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mkdir ~/work/C_MyLib  ~/work/Eclipse_Paho 
+cp ./C_MyLib/* ~/work/C_MyLib/ -rf
+cp ./Eclipse_Paho/* ~/work/Eclipse_Paho/ -rf
+
 clear
 rm ~/work/mqtt_exe/ -rf
 mkdir ~/work/mqtt_exe
